cac8d732157c3b7e3534d6badd16ea8b1f14999e
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-08 15:28:10 +0300

    update main.js

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -44,12 +44,11 @@ console.log(director1);
 
 // --------------- Task 3: printTeacher Function -----------------
 interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+  (teacher: {firstName: string, lastName: string}): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+function printTeacher({firstName, lastName}:{firstName: string; lastName: string}): string {
   return `${firstName.charAt(0)}. ${lastName}`;
-};
+}
 
-
-console.log(printTeacher("John", "Doe"));
+console.log(printTeacher({firstName: "John", lastName: "Doe"}));
