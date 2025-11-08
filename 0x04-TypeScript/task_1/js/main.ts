8741e1d980b5f96d48e5359ef131aad890ab60e6
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-08 14:18:25 +0300

    update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,3 +17,39 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+
+// Define Directors interface that extends Teacher
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// should print
+// Object
+// firstName: "John"
+// fullTimeEmployee: true
+// lastName: "Doe"
+// location: "London"
+// numberOfReports: 17
+
+
+// --------------- Task 3: printTeacher Function -----------------
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+
+console.log(printTeacher("John", "Doe"));
