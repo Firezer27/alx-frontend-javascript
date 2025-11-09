eb3e960901f2d8d894c85f706b06c068aac0efe2
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-09 10:33:39 +0300

    update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.js
+++ 0x04-TypeScript/task_3/js/crud.js
@@ -1,5 +1,3 @@
-
-
 export function insertRow(row) {
   console.log("Insert row", row);
   return Math.floor(Math.random() * Math.floor(1000));
@@ -7,7 +5,6 @@ export function insertRow(row) {
 
 export function deleteRow(rowId) {
   console.log("Delete row id", rowId);
-  return;
 }
 
 export function updateRow(rowId, row) {
--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,7 +1,3 @@
-
-
-/// <reference path="./crud.d.ts" />
-
 import { RowID, RowElement } from "./interface";
 import * as CRUD from "./crud";
 
@@ -13,9 +9,8 @@ const row: RowElement = {
 
 
 const newRowID: RowID = CRUD.insertRow(row);
-
-
 const updatedRow: RowElement = { ...row, age: 23 };
+
 CRUD.updateRow(newRowID, updatedRow);
 
 CRUD.deleteRow(newRowID);
