91dd3ff91a06ce0b7770a83c961814a905625ee2
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-09 17:40:36 +0300

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,14 +1,12 @@
 
 type Subjects = "Math" | "History";
 
-
 function teachClass(todayClass: Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
-  } else if (todayClass === "History") {
+  } else {
     return "Teaching History";
   }
-
 }
 
 
