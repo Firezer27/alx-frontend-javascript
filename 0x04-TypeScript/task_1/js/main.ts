36483a22099078bea5d7d60165305436269efd36
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-09 17:44:08 +0300

    update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,87 +1,11 @@
-// Define Teacher interface
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
-}
-
-const teacher3: Teacher = {
-  firstName: "John",
-  lastName: "Doe",
-  fullTimeEmployee: false,
-  location: "London",
-  contract: false, //
-};
-
-console.log(teacher3);
-
-
-// Define Directors interface that extends Teacher
-
-interface Director extends Teacher {
-  numberOfReports: number;
-}
-const director1: Director = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
-
-// should print
-// Object
-// firstName: "John"
-// fullTimeEmployee: true
-// lastName: "Doe"
-// location: "London"
-// numberOfReports: 17
-
-
-
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+
 function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName}. ${lastName}`;
+  return `${firstName.charAt(0)}. ${lastName}`;
 }
+
 
 console.log(printTeacher("John", "Doe"));
-
-
-// Interface describing the constructor arguments
-interface StudentConstructor {
-  firstName: string;
-  lastName: string;
-}
-
-interface StudentInterface {
-  workOnHomework(): string;
-  displayName(): string;
-}
-class StudentClass {
-  firstName: string;
-  lastName: string;
-
-  constructor({ firstName, lastName }: StudentConstructor) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework(): string {
-    return "Currently working";
-  }
-
-  displayName(): string {
-    return this.firstName;
-  }
-}
-
-const student = new StudentClass({ firstName: "John", lastName: "Doe" });
-console.log(student.displayName());
-console.log(student.workOnHomework());
