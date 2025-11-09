ee6e6194be0078bc5f844e836a6ba4d1deb0dce3
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-09 03:40:59 +0300

    update main.js file

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,4 +1,4 @@
-// Create DirectorInterface
+//task-5
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
@@ -58,71 +58,12 @@ console.log(createEmployee(1000));
 console.log(createEmployee("$500"));
 
 
-// DirectorInterface
-interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
-
-// TeacherInterface
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-// Director class
-class Director implements DirectorInterface {
-  workFromHome(): string {
-    return "Working from home";
-  }
-
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";
-  }
-
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
-  }
-}
-
-// Teacher class
-class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return "Cannot work from home";
-  }
-
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
-  }
-
-  workTeacherTasks(): string {
-    return "Getting to work";
-  }
-}
-
-// Function to create an employee
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number') {
-    if (salary < 500) {
-      return new Teacher();
-    } else {
-      return new Director();
-    }
-  } else {
-    return new Director();
-  }
-}
-
-//  Task 6 — Creating functions specific to employees
-
-
+// task-6 -  — Creating functions specific to employees
 function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
-// Function executeWork
+
 function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
@@ -133,22 +74,3 @@ function executeWork(employee: Director | Teacher): string {
 
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
-
-
-
-//  Task 7 - String literal types
-
-
-type Subjects = "Math" | "History";
-
-
-function teachClass(todayClass: Subjects): string {
-  if (todayClass === "Math") {
-    return "Teaching Math";
-  } else {
-    return "Teaching History";
-  }
-}
-
-console.log(teachClass("Math"));
-console.log(teachClass("History"));
