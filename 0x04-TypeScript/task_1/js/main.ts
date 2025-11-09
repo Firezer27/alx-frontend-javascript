e71b41dfa3f53a3abe513d1516897858279fb969
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-09 17:46:12 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,10 +2,8 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-
 function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`;
+    return firstName.charAt(0) + ". " + lastName;
+
 }
-
-
 console.log(printTeacher("John", "Doe"));
