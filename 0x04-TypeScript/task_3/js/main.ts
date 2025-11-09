e11c29ae814e28b427618a0665f9ac4b05c5165f
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-09 10:52:07 +0300

    update crud.d.ts and main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,16 +1,9 @@
-import { RowID, RowElement } from "./interface";
+/// <reference path="./crud.d.ts" />
+import { RowElement } from "./interface";
 import * as CRUD from "./crud";
 
-
-const row: RowElement = {
-  firstName: "Guillaume",
-  lastName: "Salva",
-};
-
-
-const newRowID: RowID = CRUD.insertRow(row);
+const row: RowElement = { firstName: "Guillaume", lastName: "Salva" };
+const newRowID: number = CRUD.insertRow(row);
 const updatedRow: RowElement = { ...row, age: 23 };
-
 CRUD.updateRow(newRowID, updatedRow);
-
 CRUD.deleteRow(newRowID);
